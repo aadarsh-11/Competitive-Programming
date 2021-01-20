65309ec0443f2aed906f0bc8488cbfa9caf5805a
Aadarsh Goyal <ag11.professional@gmail.com>
2021-01-20 12:15:00 +0530

    Add solution for problem `A - LCM Challenge` Link: https://codeforces.com/contest/235/problem/A Tags: number theory, *1600 Ref: https://codeforces.com/contest/235/submission/104878111

---
+++ codeforces/235/A.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fab(a,b,i) for(int i=a; i<b; i++)
+#define ll long long int
+#define ul unsigned long long int
+#define pb push_back
+#define eb emplace_back
+#define db double
+#define mp make_pair
+#define mt make_tuple
+#define endl "\n"
+#define f first
+#define se second
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+#define boost ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+ll lcm(ll x, ll y) {ll res = (x * y) / __gcd(x, y); return res;}
+
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n < 3)
+    {
+        cout << n;
+        return;
+    }
+
+    if (n % 2)
+    {
+        cout << lcm(lcm(n, (n - 1)), (n - 2));
+    }
+    else
+    {
+        ll x=n,y;
+        while(x--)
+        {
+            if(__gcd(x,n) == 1)
+            {
+                break;
+            }
+        }
+        y = x;
+        while(y--)
+        {
+            if(__gcd(y,x) == 1 && __gcd(y,n) == 1)
+            {
+                break;
+            }
+        }
+        cout << max(lcm(lcm(n, x), y), lcm(lcm(n - 1, (n - 3)), (n - 2)));
+    }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("D:/CODING/C++/Sublime Text/input.txt", "r", stdin);
+    freopen("D:/CODING/C++/Sublime Text/output.txt", "w", stdout);
+#endif
+
+    boost;
+
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
