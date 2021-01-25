b77a06efe176dbc20dfcc65417cd0c31ba62bc96
Aadarsh Goyal <ag11.professional@gmail.com>
2021-01-25 20:35:00 +0530

    Add solution for problem `A - Odd Divisor` Link: https://codeforces.com/contest/1475/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1475/submission/105314646

---
+++ codeforces/1475/A.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fab(a,b,i) for(int i=a; i<b; i++)
+#define ll long long int
+#define ul unsigned long long int
+#define pb push_back
+#define eb emplace_back
+#define db double
+#define mp make_pair
+#define mt make_tuple
+#define endl "\n"
+#define f first
+#define se second
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+#define boost ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+ll lcm(ll x, ll y) {ll res = (x * y) / __gcd(x, y); return res;}
+
+bool isPrime(int n) 
+{ 
+    // Corner case 
+    if (n <= 1) 
+        return false; 
+  
+    // Check from 2 to n-1 
+    for (int i = 2; i < n; i++) 
+        if (n % i == 0) 
+            return false; 
+  
+    return true; 
+} 
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if(n%2)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        while(n%2 == 0)
+        {
+            n = n/2;
+        }
+        if(n == 1)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+    }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("D:/CODING/C++/Sublime Text/input.txt", "r", stdin);
+    freopen("D:/CODING/C++/Sublime Text/output.txt", "w", stdout);
+#endif
+
+    boost;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
