aa60633b024d253dee4cf4a97ecb664272ae6d13
Aadarsh Goyal <ag11.professional@gmail.com>
2021-05-19 22:13:00 +0530

    Add solution for problem `A - Little Elephant and Problem` Link: https://codeforces.com/contest/220/problem/A Tags: implementation, sortings, *1300 Ref: https://codeforces.com/contest/220/submission/116677637

---
+++ codeforces/220/A.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fab(a,b,i) for(int i=a; i<b; i++)
+#define ll long long int
+#define ul unsigned long long int
+#define pb push_back
+#define eb emplace_back
+#define db double
+#define mp make_pair
+#define mt make_tuple
+#define endl "\n"
+#define f first
+#define se second
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+#define boost ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+ll lcm(ll x, ll y) {ll res = (x * y) / __gcd(x, y); return res;}
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int b[n];
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>a[i];
+        b[i] = a[i];
+    }
+    sort(b,b+n);
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if(a[i] != b[i])
+        {
+            ans++;
+        }
+    }
+    cout<<(ans==2 || ans == 0?"YES":"NO")<<endl;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("D:/CODING/C++/Sublime Text/input.txt", "r", stdin);
+    freopen("D:/CODING/C++/Sublime Text/output.txt", "w", stdout);
+#endif
+
+    boost;
+
+    int t = 1;
+
+    while (t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
